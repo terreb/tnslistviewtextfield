0d76a2d177b273989c16909f379a972f7063a74a
Alex Mantsurov <midgardcollapsed@gmail.com>
2018-05-15 16:49:21 +0200

    second commit

--- app/main-page.ts
+++ app/main-page.ts
@@ -2,12 +2,15 @@ import { EventData } from 'data/observable';
 import { Page } from 'ui/page';
 import { HelloWorldModel } from './main-view-model';
 
+let page
+
 export function navigatingTo(args: EventData) {
-    let page = <Page>args.object;
+    page = <Page>args.object;
     page.bindingContext = new HelloWorldModel();
 }
+
 let count = 0
-export function onBlur() {
+export function onBlur( args ) {
     count++
     console.log( `onBlur ${count}`)
 }
